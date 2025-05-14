d51a2117072d4decfaadf4369ce51980db57bf40
Amaresh Maharana <amareshcoder@gmail.com>
2025-05-14 20:22:56 +0530

    refactor Navbar component to include navigation links and icons; remove unnecessary whitespace from Home component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+
+const navIcons = [
+  {
+    src: '/assets/icons/search.svg', alt: 'search'
+  },
+  {
+    src: '/assets/icons/black-heart.svg', alt: 'heart'
+  },
+  {
+    src: '/assets/icons/user.svg', alt: 'user'
+  },
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <header className='w-full'>
+        <nav className='nav'>
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/assets/icons/logo.svg" alt="Logo" width={27} height={27} />
+            <p className="nav-logo">
+              Pri<span className="text-primary">Cio</span>
+            </p>
+          </Link>
+          <div className="flex items-center gap-5">
+            {navIcons.map((icon) => (
+              <Image key={icon.alt} src={icon.src} alt={icon.alt} width={28} height={28} className="object-contain cursor-pointer" />
+            ))}
+          </div>
+        </nav>
+      </header>
+    </>
   )
 }
 
