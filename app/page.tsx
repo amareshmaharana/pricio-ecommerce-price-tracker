b496de407d4574f0b1cccbcdbdcb60654d921de1
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-18 17:21:37 +0530

    Refactor Home component for consistent styling and improved readability

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,15 @@ const Home = async () => {
     <>
       <section className="px-6 md:px-20 py-4 2xl:px-44">
         <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center">
+          <div className="flex flex-col justify-center font-spaceGrotesk">
             <p className="small-text">
               Smart shopping starts from here:
               <Image src="/assets/icons/arrow-right.svg" alt="arrow_right" width={16} height={16} />
             </p>
 
             <h1 className="head-text">
-              Unleash the power of
-              <span className="text-primary"> PriCio</span>
+              Unleash the power of 
+              Pri<span className="text-primary">Cio</span>
             </h1>
             <p className="mt-6">
               Powerful, self-serve product and growth analytics help u to connect, engage, and retain more.
@@ -32,7 +32,7 @@ const Home = async () => {
       </section>
 
       <section className="trending-section">
-        <h2 className="section-text">Trending</h2>
+        <h2 className="section-text font-spaceGrotesk">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product) => (
