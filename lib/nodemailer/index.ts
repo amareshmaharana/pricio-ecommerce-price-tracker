9a04fb6b6b6b91da0214f83d8202bb93840cdff7
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-23 18:33:07 +0530

    Update email branding and sender address in nodemailer integration

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -29,7 +29,7 @@ export async function generateEmailBody(
       subject = `Welcome to Price Tracking for ${shortenedTitle}`;
       body = `
         <div>
-          <h2>Welcome to PriceWise 🚀</h2>
+          <h2>Welcome to PriCio 🚀</h2>
           <p>You are now tracking ${product.title}.</p>
           <p>Here's an example of how you'll receive updates:</p>
           <div style="border: 1px solid #ccc; padding: 10px; background-color: #f8f8f8;">
@@ -85,7 +85,7 @@ const transporter = nodemailer.createTransport({
   service: "hotmail",
   port: 2525,
   auth: {
-    user: "javascriptmastery@outlook.com",
+    user: "priciotrack@outlook.com",
     pass: process.env.EMAIL_PASSWORD,
   },
   maxConnections: 1,
@@ -96,7 +96,7 @@ export const sendEmail = async (
   sendTo: string[]
 ) => {
   const mailOptions = {
-    from: "javascriptmastery@outlook.com",
+    from: "priciotrack@outlook.com",
     to: sendTo,
     html: emailContent.body,
     subject: emailContent.subject,
